f8595d7a7ffb415f11e70afc9726f47d4cd9df36
SakuraMH <132273042+SakuraMH@users.noreply.github.com>
2023-10-23 00:56:36 +0200

    Add files via upload

---
+++ billing/billing/src/test/java/org/sdia/billing/BillingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sdia.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
